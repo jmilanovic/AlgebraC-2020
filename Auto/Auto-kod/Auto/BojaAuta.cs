31d9e3723185e11a3df718250fcc68087d63a056
jmilanovic <josip.milanovic@net.hr>
2021-02-12 14:23:52 +0100

    test

---
+++ Auto/Auto-kod/Auto/BojaAuta.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Auto
+{
+    class BojaAuta
+    {
+        public string boja="plava";
+        public int KS=45;
+
+        public override string ToString()
+        {
+            // return base.ToString();
+            return "Ja sam Automobil Audi A1, moja boja je: " + 
+                this.boja+" Konjska snaga je:"+this.KS;
+         }
+
+        internal void DodajSnagu(int v)
+        {
+            this.KS = 150;
+        }
+    }
+}
---
+++ Auto/Auto-kod/Auto/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+/*Ovo je jedan primjer gdje se pozivaju varijable koje su u drugoj klasi
+  te se istoj klasi šalje neka vrijednost varijable iz druge/osnovne klase 
+ */
+namespace Auto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ispiši");
+
+            BojaAuta a1 = new BojaAuta();
+            Console.WriteLine(a1);
+            a1.boja = "zelena";
+            Console.WriteLine(a1);
+            //a1.c = "z";
+            a1.DodajSnagu(200);
+            Console.WriteLine("Konjska snaga nakon ulaska u metodu DodajSnagu je: "+a1.KS);
+
+                
+
+
+
+        }
+    }
+}
+
+namespace Auto1
+{
+    class Program
+    {
+        static void Main2(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ispiši");
+
+
+
+        }
+    }
+}
